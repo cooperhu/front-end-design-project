d59dd13c578febe921159616b54abbb88dd041cc
Connor Hubeny <chubeny@icloud.com>
2018-05-11 17:08:43 -0400

    Add Gallery items info

---
+++ nbcux-design/src/GalleryItems.js
@@ -0,0 +1,84 @@
+export default {
+  imgObjects: [
+    {
+      title: 'Image #01',
+      path: '/assets/img/1.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #02',
+      path: '/assets/img/2.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #03',
+      path: '/assets/img/3.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #04',
+      path: '/assets/img/4.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #05',
+      path: '/assets/img/5.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #06',
+      path: '/assets/img/6.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #07',
+      path: '/assets/img/7.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #08',
+      path: '/assets/img/8.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #09',
+      path: '/assets/img/1.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #10',
+      path: '/assets/img/2.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #11',
+      path: '/assets/img/3.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #12',
+      path: '/assets/img/4.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #13',
+      path: '/assets/img/5.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #14',
+      path: '/assets/img/6.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #15',
+      path: '/assets/img/7.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    },
+    {
+      title: 'Image #16',
+      path: '/assets/img/8.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ex a massa ornare placerat. Morbi ac semper odio. Maecenas ipsum elit, dictum elementum justo vitae, tincidunt tempor sem. Integer a sem eu nunc varius ultricies nec in massa. Sed blandit dui nisi, ac faucibus risus ultrices vel.',
+    }
+  ]
+};
